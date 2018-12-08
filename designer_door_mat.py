9cb127867dae4f162e64c373c1d29ff7ca2f991a
Ivan Kovalenko <kovalenko_ivan@hotmail.com>
2018-12-09 00:28:23 +0300

    divide problem statement and solution correspondingly in .md and .py files

--- designer_door_mat.py
+++ designer_door_mat.py
@@ -1,31 +1,4 @@
-'''Mr. Vincent works in a door mat manufacturing company. One day, he
-designed a new door mat with the following specifications:
-
-Mat size must be N X M. (N is an odd natural number, and M is 3 times N.)
-
-The design should have 'WELCOME' written in the center.
-The design pattern should only use |, . and - characters.
-
-Sample Designs
-    Size: 7 x 21 
-    ---------.|.---------
-    ------.|..|..|.------
-    ---.|..|..|..|..|.---
-    -------WELCOME-------
-    ---.|..|..|..|..|.---
-    ------.|..|..|.------
-    ---------.|.---------
-
-Input Format
-A single line containing the space separated values of  and .
-
-Constraints
-5 < N < 101
-15 < M < 303
-
-Output Format
-Output the design pattern.
-'''
+#!/bin/python3
 
 N, M = [int(x) for x in input().split()]
 
